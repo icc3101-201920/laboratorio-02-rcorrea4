44053a6ee28f388c57a8cb2e9176c8a0211c3295
rcorrea4 <rcorrea4@miuandes.cl>
2019-08-16 20:30:40 -0400

    a

--- Laboratorio_1_OOP_201902/Hand.cs
+++ Laboratorio_1_OOP_201902/Hand.cs
@@ -5,24 +5,25 @@ using System.Text;
 
 namespace Laboratorio_1_OOP_201902
 {
-    public class Hand
+    public class Hand:Deck
     {
-        private List<CombatCard> combatCards;
-        private List<SpecialCard> SpecialCards;
-
-        public Hand()
+        public Hand(List<SpecialCard> specialcards, List<CombatCard> combatcards) : base(specialcards,combatcards)
         {
 
         }
-
-        public List<CombatCard> CombatCards { get => combatCards; set => combatCards = value; }
-        public List<SpecialCard> SpecialCards1 { get => SpecialCards; set => SpecialCards = value; }
+        
 
         
-        public void AddCombatCard(CombatCard combatCard) { throw new NotImplementedException(); }
-        public void AddSpecialCard(SpecialCard specialCard) { throw new NotImplementedException(); }
-        public void DestroyCombatCard(int cardId) { throw new NotImplementedException(); }
-        public void DestroySpecialCard(int cardId) { throw new NotImplementedException(); }
+
+        
+
+
+
+
+       
+
+        
+        
         
     }
 }
